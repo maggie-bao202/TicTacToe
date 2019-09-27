113be33de11c521a9264a927168a7795ac38813a
U-BSD\463293 <463293@SHS-10DEC7FA8.beaverton.k12.or.us>
2019-09-27 09:13:33 -0700

    works, need to work on play again

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void printBoard();
-bool checkWin();
+bool checkWin(int);
 bool checkTie();
 
 int board [3][3];
@@ -13,53 +13,110 @@ const int X_MOVE = 1;
 const int O_MOVE = 2;
 const int X_TURN = 0;
 const int O_TURN = 1;
+int turn = X_TURN;
 int xcounter = 0;
 int ocounter = 0;
 int tcounter = 0;
 char str[80];
+bool stillPlaying = true;
+char response = ' ';
+
 int main()
 {
-  cin.get(str, 80);
-  cin.get();
-  for (int i = 0; i <strlen(str); i++){
-    if (str[2] != "\0"){
+  while (stillPlaying == true){
+    while (checkWin(X_MOVE) == false && checkWin(O_MOVE) == false && checkTie() == false){
       cout << "Please enter in a letter followed by a number." << endl;
+      printBoard();
+      cin.get(str, 80);
+      cin.clear();
+      cin.ignore(9999, '\n');
+      if (strlen(str) != 2){
+        cout << "Please enter in a letter followed by a number." << endl;
+      }
+      else if (str[0] != 'a' && str[0] != 'b' && str[0] != 'c'){
+        cout << "Row must be letter a, b, or c." << endl;
+      }
+      else if (str[1] != '1' && str[1] != '2' && str[1] != '3'){
+        cout << "Columns must be numbers 1, 2, or 3." << endl;
+      }
+      else{
+        int row = str[0] - 'a';
+        int column = str[1] - '1';
+        if (board[row][column] == BLANK){
+ 	  if (turn == X_TURN){
+	    board[row][column] = X_MOVE;
+	    turn = O_TURN;
+	  }
+  	  else{
+	    board[row][column] = O_MOVE;
+	    turn = X_TURN;
+  	  }
+        }
+        else {
+	  cout << "There's a piece there!" << endl;
+        }
+      }
+      if (checkWin(X_MOVE) == true) {
+        cout << "X wins!" << endl;
+        xcounter++;
+      }
+      else if (checkWin(O_MOVE) == true) {
+        cout << "O wins!" << endl;
+        ocounter++;
+      }
+      else if (checkTie() == true) {
+	cout << "It is a tie!" << endl;
+	tcounter++;
+      }
     }
-    else if (str[0] != 'a' || str[0] != 'b' || str[0] != 'c'){
-      cout << "Row must be letter a, b, or c." << endl;
+      cout << "X has " << xcounter << " wins." << endl;
+      cout << "O has " << ocounter << " wins." << endl;
+      cout << "There has been " << tcounter << " ties." << endl;
+      bool ask = true;
+      while (ask == true) {
+        cout << "Play again? (y/n)" << endl;
+        cin >> response;
+        if (response == 'n' || response == 'N'){
+          cout << "Have a nice day!" << endl;
+	  ask = false;
+          stillPlaying = false;
+          break;
+        }
+        else if (response == 'y' || response == 'Y'){
+  	  ask = false;
+	  for (int row = 0; row < 3; row ++){
+	    for (int column = 0; column < 3; column++) {
+	      board[row][column] = BLANK;
+	      stillPlaying = true;
+	    }
+	  }
+	  break;
+        }
+        else {
+	  cout << "Input must be either y or n." << endl;
+	  ask = true;
+        }
+      }
     }
-    else if (str[0] != '1' || str[0] != '2' || str[0] != '3'){
-      cout << "Columns must be numbers 1, 2, or 3." << endl;
-    }
-    else{
-      int row = str[0] - 'a';
-      int column = str[1] - '1';
-      if (board[row][column] == BLANK){
-	if (turn == X_TURN){
-	  board[row][column] = X_MOVE;
-	  turn = O_TURN;
-    }
-  }
-  printBoard();
-  return 0;
+    return 0;
 }  
 
 void printBoard(){
-  cout <<  << endl;
+  cout <<"\t1\t2\t3 "  << endl;
   for(int row = 0; row < 3; row++){
-    cout << (char)('a' + row) << endl;
+    cout << (char)('a' + row) << "\t";
     for(int column = 0; column < 3; column++){
-    //if(board[row][column] == BLANK){
-	cout <<"   " << " ";
-	//}
-	/* else if (board[row][column] == X_MOVE){
-	cout << "X  " << " ";
-
+      if(board[row][column] == BLANK){
+	cout << "\t";
+      }
+      else if (board[row][column] == X_MOVE){
+	cout << "X\t";
       }
       else if (board[row][column] == O_MOVE){
-	cout << "O  " << " ";
-	}*/
+	cout << "O\t";
+      }
     }
+    cout << " " << endl;
   }
 }
   
