7d54867990f1bded830ff31fcacc2206358fdab3
U-BSD\463293 <463293@SHS-10DEC7FA8.beaverton.k12.or.us>
2019-10-03 08:21:45 -0700

    Finished comments

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+//for methods, declare first before initialization
 void printBoard();
 bool checkWin(int);
 bool checkTie();
@@ -28,22 +29,22 @@ int main()
   while (stillPlaying == true){
     while (checkWin(X_MOVE) == false && checkWin(O_MOVE) == false && checkTie() == false){
       cout << "Please enter in a lowercase letter followed by a number (ex. \"a1\", \"c3\")." << endl;
-      printBoard();
-      cin.get(str, 5);
-      cin.clear();
+      printBoard(); //print empty gridboard
+      cin.get(str, 5); //put into array of 5. Extra char will be ignored
+      cin.clear(); //clear, ignore fixes null bug
       cin.ignore(9999, '\n');
-      if (strlen(str) != 2){
+      if (strlen(str) != 2){//if length is not 2, loop to cout above
       }
-      else if (str[0] != 'a' && str[0] != 'b' && str[0] != 'c'){
+      else if (str[0] != 'a' && str[0] != 'b' && str[0] != 'c'){//array index 0
         cout << "Row must be letter a, b, or c." << endl;
       }
-      else if (str[1] != '1' && str[1] != '2' && str[1] != '3'){
+      else if (str[1] != '1' && str[1] != '2' && str[1] != '3'){//array index 1
         cout << "Column must be number 1, 2, or 3." << endl;
       }
       else{
-        int row = str[0] - 'a';
-        int column = str[1] - '1';
-        if (board[row][column] == BLANK){
+        int row = str[0] - 'a'; //set index of row
+        int column = str[1] - '1';//set index of column
+        if (board[row][column] == BLANK){//if the box is blank
  	  if (turn == X_TURN){
 	    board[row][column] = X_MOVE;
 	    turn = O_TURN;
@@ -57,15 +58,15 @@ int main()
 	  cout << "There's a piece there!" << endl;
         }
       }
-      if (checkWin(X_MOVE) == true) {
+      if (checkWin(X_MOVE) == true) { //If X move
         cout << "X wins!" << endl;
-        xcounter++;
+        xcounter++; //add 1 to counter
       }
-      else if (checkWin(O_MOVE) == true) {
+      else if (checkWin(O_MOVE) == true) { //If O move
         cout << "O wins!" << endl;
         ocounter++;
       }
-      else if (checkTie() == true) {
+      else if (checkTie() == true) { //tie method below
 	cout << "It is a tie!" << endl;
 	tcounter++;
       }
@@ -74,18 +75,18 @@ int main()
       cout << "O has " << ocounter << " wins." << endl;
       cout << "There has been " << tcounter << " ties." << endl;
       bool ask = true;
-      while (ask == true) {
+      while (ask == true) {//create loop for if response is not valid input
         cout << "Play again? (y/n)" << endl;
         cin >> response;
         if (response == 'n' || response == 'N'){
           cout << "Have a nice day!" << endl;
 	  ask = false;
           stillPlaying = false;
-          exit(0);
+          exit(0); //close program
         }
         else if (response == 'y' || response == 'Y'){
   	  ask = false;
-	  for (int row = 0; row < 3; row ++){
+	  for (int row = 0; row < 3; row ++){ //reset to a blank board
 	    for (int column = 0; column < 3; column++) {
 	      board[row][column] = BLANK;
 	    }
@@ -95,31 +96,31 @@ int main()
 	  cout << "Input must be either \"y\" or \"n\"." << endl;
         }
       }
-      cin.get();
+      cin.get(); //prevent empty board from printing twice
     }
     return 0;
 }  
 
 void printBoard(){
-  cout <<"\t1\t2\t3 "  << endl;
+  cout <<"\t1\t2\t3 "  << endl; //print header
   for(int row = 0; row < 3; row++){
-    cout << (char)('a' + row) << "\t";
+    cout << (char)('a' + row) << "\t"; //letter and space
     for(int column = 0; column < 3; column++){
-      if(board[row][column] == BLANK){
+      if(board[row][column] == BLANK){ //if box is blank
 	cout << "\t";
       }
-      else if (board[row][column] == X_MOVE){
+      else if (board[row][column] == X_MOVE){ //if x calls the shots
 	cout << "X\t";
       }
-      else if (board[row][column] == O_MOVE){
+      else if (board[row][column] == O_MOVE){ //if o calls the shots
 	cout << "O\t";
       }
     }
-    cout << " " << endl;
+    cout << " " << endl; //new row
   }
 }
   
-bool checkWin (int player){
+bool checkWin (int player){ //checks all possible combinations of a win in two dimentional array
   if (board[0][0] == player && board[0][1] == player && board[0][2] == player){
     return true;
   }
@@ -148,7 +149,7 @@ bool checkWin (int player){
 }
 
  
-bool checkTie(){
+bool checkTie(){ //if all boxes are filled, return true
   for (int row = 0; row < 3; row++){
     for (int column = 0; column < 3; column++){
       if(board[row][column] == BLANK){
